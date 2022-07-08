769ef60b91c034eb79adaed378e859f62923ed95
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-07-08 12:28:53 -0400

    Create Models test

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :post_count
 
   def five_last_comments
